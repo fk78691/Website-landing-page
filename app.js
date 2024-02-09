e96a1d643d5f3c6d2e5a6a794b8dabfb8d093719
Fahad Khan <138850947+fk78691@users.noreply.github.com>
2024-02-09 11:57:02 +0500

    commit

---
+++ app.js
@@ -0,0 +1,17 @@
+gsap.from("#left ",{
+ delay:0.2,
+ opacity:0,
+ duration:1,
+})
+gsap.from("#right ",{
+    delay:0.2,
+    opacity:0,
+    duration:1,
+   })
+
+   gsap.to(button,{
+    delay:0.2,
+    opacity:0,
+    duration:2,
+    
+   })
